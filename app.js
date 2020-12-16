c56259c72198c93f670514bfb67eb4db4e7daa64
abhay-tank <abhay.tank@raweng.com>
2020-12-16 13:06:27 +0530

    Changed config settings

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 
 // Check if its production server or development server
 // If development server -> use configuration from config.env
-if (!process.env.NODE_ENV && !process.env.NODE_ENV == "production") {
+if (!process.env.NODE_ENV || !process.env.NODE_ENV == "production") {
 	dotenv.config({ path: path.join(__dirname, "config.env") });
 }
 const { config } = require("./src/configuration/config");
